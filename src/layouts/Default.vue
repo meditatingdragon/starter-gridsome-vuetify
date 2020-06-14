d8e710a1abd1d15861944ce45bd0e788e223d07d
Rachel <rachel@meditatingdragon.com>
2020-06-14 15:24:36 -0500

    update gridsome version

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,34 +1,31 @@
 <template>
   <v-app>
-    <Header/>
+    <Header />
     <Hero v-if="isHome" />
     <v-container fill-height class="d-sm-none d-md-flex">
-      <slot/>
+      <slot />
     </v-container>
-    <Footer/>
+    <Footer />
   </v-app>
 </template>
 
 <script>
-import Header from '../components/Header'
-import Hero from '../components/Hero'
-import Footer from '../components/Footer'
+import Header from "../components/Header"
+import Hero from "../components/Hero"
+import Footer from "../components/Footer"
 
 export default {
   components: {
     Header,
     Hero,
-    Footer
+    Footer,
   },
   computed: {
     isHome() {
-      return this.$route.path === '/';
-    }
-  }
+      return this.$route.path === "/"
+    },
+  },
 }
 </script>
 
-
-<style>
-
-</style>
+<style></style>
